0120166544a7c146e54dc6725c7196875f316342
Baccon <bachcong.hn@gmail.com>
2025-07-01 16:40:23 +0700

    feat: added some backend authentication

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import products from "./data/products.js";
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
@@ -15,6 +16,9 @@ const app = express();
 //body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Cookie parser middleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
